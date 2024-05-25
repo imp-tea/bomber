f32867efdb2044f074e7abf94921a3ea91cfc42a
imp-tea <adam.m.huddleston@gmail.com>
2024-05-25 10:19:21 -0500

    Added default cannon to test level.

--- main.lua
+++ main.lua
@@ -5,6 +5,7 @@ require 'Entity'
 require 'Camera'
 require 'Player'
 require 'Bomb'
+require 'Cannon'
 require 'util'
 
 math.randomseed(os.time())
@@ -28,7 +29,8 @@ function love.load()
     trampolineL = Terrain:new(World,WIDTH-80,HEIGHT-180,{restitution = 2, type = 'polygon', points = {80,-80,80,80,-80,80}, color = util.randomColor(theme)})
 
     player = Player:new(World,WIDTH/2,HEIGHT/3,{group = 1, fixedRotation = true, ungroundedMultiplier = 0.2, height = 50, width = 25, color = util.randomColor(theme), shape='pill'})
-    
+    cannon = Cannon:new(World, WIDTH/3, 200)
+
     platform = Obstacle:new(World, WIDTH/2+100, HEIGHT-150, {
         width = 150,
         height = 30,
